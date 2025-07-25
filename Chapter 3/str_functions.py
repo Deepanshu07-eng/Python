b0599cb43f422b8d3050192fff872ee6b734c554
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-25 20:41:29 +0530

    String functions

---
+++ Chapter 3/str_functions.py
@@ -0,0 +1,18 @@
+#1 len function len()
+
+name= "Deepanshu"
+
+print(len(name))
+
+#2 endswith function endswith()
+
+print(name.endswith("shu")) #true
+print(name.endswith("dee"))#false
+
+
+print(name.startswith("Dee")) #true
+print(name.startswith("ans"))#false
+
+#Rest of the functions are in the Python handbook.
+
+
