c2606057b3fefb5a5630c88680dc2663cd15c303
Deepanshu07-eng <d2004gautam@gmail.com>
2025-07-25 20:12:00 +0530

    String pt 2

--- Chapter 3/intro_to_string.py
+++ Chapter 3/intro_to_string.py
@@ -5,3 +5,12 @@ name= 'Rahul'  # single quotes
 name = "Rahul" # double quotes
 
 name = '''Rahul''' # triple quote
+
+
+a="Deepanshu"
+
+namesort = a[0:3] #start from index 0 all the way till 3 (excluding 0)
+print(namesort)
+
+character1 =a[1]
+print(character1)
