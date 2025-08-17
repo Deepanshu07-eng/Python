7ead0e93c573fff46838850a475e640225eeb098
Deepanshu <d2004gautam@gmail.com>
2025-08-17 19:42:21 +0530

    List and some list methods

--- Chapter 4 List and Tupples/01.List.py
+++ Chapter 4 List and Tupples/01.List.py
@@ -1,3 +1,8 @@
 ForExample = ["Rahul", "Gautam", 716, 12.32, True, False, "Red", "blue", "Apple"]
 
 print(ForExample[4])
+
+ForExample[0] ="Deepanshu"
+
+print(ForExample[0])
+print(ForExample[1:4])
---
+++ Chapter 4 List and Tupples/List_Methods.py
@@ -0,0 +1,14 @@
+example = ["Rahul", "Gautam", 716, 12.32, True, False, "Red", "blue", "Apple"]
+print(example)
+example.append("Coding")
+print(example)
+
+
+l1 = [42,45,62,15,63,21,62,11,93]
+
+l1.sort()
+print(l1)
+
+l1.reverse()
+l1.insert(4,100)
+print(l1)
