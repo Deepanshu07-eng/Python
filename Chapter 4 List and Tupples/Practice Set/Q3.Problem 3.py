724718bb416c6f32aa271ff044fd6fda1bd5a244
Deepanshu <d2004gautam@gmail.com>
2025-08-18 20:15:46 +0530

    Some practice Questions for list and tupple

---
+++ Chapter 4 List and Tupples/Practice Set/Q3.Problem 3.py
@@ -0,0 +1,6 @@
+a = (34, 543, " Deepanshu")
+
+a(2) = "Deepanshu"
+
+#tupple is immutable, it can't be changed
+
---
+++ Chapter 4 List and Tupples/Practice Set/Q4. Problem 4.py
@@ -0,0 +1,3 @@
+list = [421, 462, 812, 331]
+
+print(sum(list)) # this function can sum all the elements located in list
