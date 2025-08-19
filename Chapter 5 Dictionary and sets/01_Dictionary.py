1f0d084df132607e106ede226bddc6d46f7b93e1
Deepanshu <d2004gautam@gmail.com>
2025-08-19 18:45:22 +0530

    Dictionary in python

---
+++ Chapter 5 Dictionary and sets/01_Dictionary.py
@@ -0,0 +1,16 @@
+marks = {
+    "Deepanshu": 100,
+    "Rohan" : 74,
+    "Rahul" :92,
+    "Karan" : 45
+}
+
+print(marks, type(marks))
+print(marks["Karan"])
+
+
+#Properties of Python Dictionaries
+# 1. It is unordered.
+# 2. It is mutable. Dictionary is changable
+# 3. It is indexed.
+# 4. Cannot contain duplicate keys
