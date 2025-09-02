a7d6e5a15e2d10e626c28147266b4e287df3dc15
Deepanshu <d2004gautam@gmail.com>
2025-09-02 19:52:23 +0530

    Q6- Q9, Easy and important problems

---
+++ Chapter 5 Dictionary and sets/Practice_set/Q6.practice6.py
@@ -0,0 +1,18 @@
+A = {}
+
+name = input("Enter Friends Name: ")
+lang = input("Enter language name: ")
+A.update({name: lang})
+
+name = input("Enter Friends Name: ")
+lang = input("Enter language name: ")
+A.update({name: lang})
+name = input("Enter Friends Name: ")
+lang = input("Enter language name: ")
+A.update({name: lang})
+
+name = input("Enter Friends Name: ")
+lang = input("Enter language name: ")
+A.update({name: lang})
+
+print(A)
---
+++ Chapter 5 Dictionary and sets/Practice_set/Q9.practice9.py
@@ -0,0 +1,7 @@
+s = {8,7,12,"Harry", [1,2]}
+
+#Set me indexing nhi hoti
+
+# No you cannot change tha variable inside a list contained in a set in python
+#in fant, you cannot even have a list an element in a set because 
+#set in python require all their elemnets to be immutable and hashable.
