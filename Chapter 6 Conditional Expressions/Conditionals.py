a737e72442592a895ec7a616fb6654acba2c926d
Deepanshu <d2004gautam@gmail.com>
2025-09-02 20:16:00 +0530

    IF- ELIF- ELSE ladder

---
+++ Chapter 6 Conditional Expressions/Conditionals.py
@@ -0,0 +1,27 @@
+# In python programming, we must be able to execute instructions on a condition(s) being met.
+
+#This is what conditions are for :-
+
+# IF, ELSE and ELIF in PYTHON
+
+a = int(input("Enter your Age: "))
+
+# IF - ELSE Ladder
+
+if(a>18):
+    print("You are Eligible for voting")
+    print("Good for you and for Congress party haha lol")
+else:
+    print("You are not Eligible for voting")
+
+
+# IF -ELSE -ELSE ladder
+
+
+if(a>18):
+    print("You are Eligible for voting")
+    print("Good for you and for Congress party haha lol")
+elif(a<0):
+    print("Go to your mumma's belly or fathers ball's back! Its your choice lol!")
+else:
+    print("You are not Eligible for voting")
