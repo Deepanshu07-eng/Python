a80cefd0b9dbaf02f794d9368c14a0f86d14297f
Deepanshu <d2004gautam@gmail.com>
2025-09-05 23:40:14 +0530

    Relational anf logical operators

---
+++ Chapter 6 Conditional Expressions/Realtional and Logical Operators.py
@@ -0,0 +1,15 @@
+# [" RELATIONAL OPERATORS"]
+
+# Realtional Operators (Also called Comparission Operators) are used to evalute conditions inside the if statement.
+# Some examples are relational Operators are:
+# '='Equals.
+# '>=' Greater than equals to.
+# '<=' Lesser Than Equals to.
+
+
+
+# [" LOGICAL OPERATORS "]
+
+# AND -> True if both operands are true else false.
+# OR -> True if atleast one operands is true or else false.
+# NOT -> Invert True to False and False to True.
