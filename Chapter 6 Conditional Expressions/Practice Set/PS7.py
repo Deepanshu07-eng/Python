83a5e8a4b56736a95b3d574b9adc729373332044
Deepanshu <d2004gautam@gmail.com>
2025-09-08 22:49:47 +0530

    Practice set 6 or Practice set 7

---
+++ Chapter 6 Conditional Expressions/Practice Set/PS7.py
@@ -0,0 +1,8 @@
+post =input("Enter the post: ")
+
+if("deepanshu" in post.lower()):
+    print("Yes! the post is talking about Deepanshu")
+   
+
+else:
+    print("Yes! the post is not talking about Deepanshu")
