5ac0dbd3f2f1ec11db1a0484ef85e849f0792ac2
Deepanshu <d2004gautam@gmail.com>
2025-09-08 22:29:33 +0530

    Problem 4 of if elif else statement

---
+++ Chapter 6 Conditional Expressions/Practice Set/PS4.py
@@ -0,0 +1,7 @@
+username = input("Enter username here")
+
+if(len(username)< 10):
+    print("Your username Contains less than 10 characters")
+
+else:
+    print("Your username contains more than 10 characters")
