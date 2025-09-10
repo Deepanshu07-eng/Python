d0cb54f8439c4d693d8bcf2cd82e63991a7e6804
Deepanshu <d2004gautam@gmail.com>
2025-09-10 18:14:15 +0530

    Break and continue in loop

---
+++ Chapter 7 Loops/Break_And_Continue.py
@@ -0,0 +1,10 @@
+for i in range(50):
+    if(i == 26):
+        break #Exit the loop right now
+    print(i)
+
+
+for i in range(50):
+    if(i == 26):
+        continue #Skip this iteration
+    print(i)
--- Chapter 7 Loops/For_with_ELse.py
+++ Chapter 7 Loops/For_with_ELse.py
@@ -1,7 +1,7 @@
-l = [1,3,4.12]
+l = [1,3,4,12]
 
 for item in l:
     print(item)
 
 else:
-    print("done")#This is printed when the loop exhausts
+    print("done")#This is printed when the loop exhausts!
