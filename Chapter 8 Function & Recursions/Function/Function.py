b1b9105f3d997b192f16d987413ed65dfd7ffe1f
Deepanshu <d2004gautam@gmail.com>
2025-09-11 20:04:34 +0530

    Function

---
+++ Chapter 8 Function & Recursions/Function/Function.py
@@ -0,0 +1,34 @@
+"""
+FUNCTIONS: A function is a group of statements performing a specific task.
+
+"""
+'''
+a = int(input("Enter the number"))
+b = int(input("Enter the number"))
+c = int(input("Enter the number"))
+
+average = (a + b + c)/3
+print(average)
+
+a = int(input("Enter the number"))
+b = int(input("Enter the number"))
+c = int(input("Enter the number"))
+
+average = (a + b + c)/3
+print(average)
+
+'''
+#Dont do like this, Do it with Function.
+
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+
+    average = (a + b + c)/3
+    print(average)
+
+avg()
+avg()
+avg()
+avg()
