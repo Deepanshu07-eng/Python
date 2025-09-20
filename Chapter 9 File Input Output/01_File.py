a5c744bd377c1ffd92a2e1a423b7874b6ed46901
Deepanshu <d2004gautam@gmail.com>
2025-09-20 19:23:25 +0530

    Reading text file

---
+++ Chapter 9 File Input Output/01_File.py
@@ -0,0 +1,24 @@
+'''
+a = " a very long string with emails "
+
+emails = []
+3 seconds
+
+The random-access memory is voilatile, and all its contents are lost once a program terminates in order to persist the data 
+forever, we use files.
+
+A file is data stored in a storage device. a python program can talk to the file by reading content
+from it and writing content to it.
+
+TYPES OF FILES.
+
+1. Text Files. (.txt, .c etc.)
+2. Binary Files. (.jpg, .dat etc.)
+'''
+
+# Text file. ( Open/ Read/ Print/ Close)
+
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
