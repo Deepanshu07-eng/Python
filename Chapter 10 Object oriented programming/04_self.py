9d5fce586bd40e437563ea79f2ee6e63f7350387
Deepanshu <d2004gautam@gmail.com>
2025-09-22 20:17:31 +0530

    Self in function

---
+++ Chapter 10 Object oriented programming/04_self.py
@@ -0,0 +1,17 @@
+class employee:
+    language = "Python" # This is a class attribute
+    salary = "1000000"
+
+    def getInfo(self):
+        print(f"The language is {self.language} and the salary is {self.salary}")
+
+    def greet(self):
+        print("Good morning")
+
+
+Deepanshu = employee()
+Deepanshu.language = "C++" # this is an Instance attribute.
+
+Deepanshu.greet()
+Deepanshu.getInfo() 
+# employee.getInfo(harry)
