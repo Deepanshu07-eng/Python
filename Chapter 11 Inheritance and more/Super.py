e1f61e3007c7465a9d31985cba43197831487978
Deepanshu <d2004gautam@gmail.com>
2025-09-28 17:37:19 +0530

    super method

---
+++ Chapter 11 Inheritance and more/Super.py
@@ -0,0 +1,24 @@
+class employee:
+    def __init__(self):
+        print("Constructor of Employee")
+    a =1
+
+class programmer(employee):
+    def __init__(self):
+        print("Constructor of Programmer")
+    b =2
+
+class manager(programmer):
+    def __init__(self):
+        super().__init__()
+        print("Constructor of Manager")
+    c =3
+
+#o= employee()
+#print(o.a)
+
+#o= programmer()
+#print(o.a, o.b)
+
+o= manager()
+print(o.a, o.b, o.c)
