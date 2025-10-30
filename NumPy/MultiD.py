0371f97d73acda8275e55b5b2f6c3309c7991ed4
Deepanshu <d2004gautam@gmail.com>
2025-10-30 22:48:04 +0530

    Multi dimensional array

---
+++ NumPy/MultiD.py
@@ -0,0 +1,24 @@
+# multi-Dimensional Array
+#matrix
+
+import numpy as np
+
+matrix = np.array([[4,5,6],
+                   [7,8,9]])
+
+print(matrix)
+
+
+'''
+OVERVIEW:
+
+# 1D Array :-
+    [1,2,3,4]
+
+# 2D Array :-
+    rows & columns
+
+# Multi-D Array :-
+    Stack of Multiple 2D Arrays
+
+'''
