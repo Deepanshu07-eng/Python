505f45f3bf4199d8d102e421f861c3dc43ca35e0
Deepanshu <d2004gautam@gmail.com>
2025-10-30 22:59:45 +0530

    With default value zero (0)

---
+++ NumPy/Creation/Cr array from py list.py
@@ -0,0 +1,10 @@
+# Creating array from python list
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+print(arr)
+
+'''
+Converted a python list to numpy array
+'''
---
+++ NumPy/Creation/With Default Value.py
@@ -0,0 +1,8 @@
+# Creating Array with default Values
+#np.zeros(shape) (3) for 1Dm  (3,3) for 2D
+
+
+import numpy as np
+
+zeroes_array = np.zeros(3)
+print(zeroes_array)
