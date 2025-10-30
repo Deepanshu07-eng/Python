b11089b3df2d022b7e41a176c2d4daa61c5c3f39
Deepanshu <d2004gautam@gmail.com>
2025-10-30 23:14:12 +0530

    Identity matrix

---
+++ NumPy/Creation/Identity metrix.py
@@ -0,0 +1,9 @@
+# creating identity matrices
+
+# it is a square matrix with 1's on the diagonal and 0's elsewhere
+#eye(size)
+import numpy as np
+
+identity_matrix = np.eye(5)
+print(identity_matrix)
+
