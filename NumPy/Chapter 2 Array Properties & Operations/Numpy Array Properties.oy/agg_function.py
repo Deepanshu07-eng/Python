de57fd83d4a2a11b51fd5d80de26d2876f9671d5
Deepanshu <d2004gautam@gmail.com>
2025-11-07 18:40:54 +0530

    Mean, min, max, std, var function

--- NumPy/Chapter 2 Array Properties & Operations/Numpy Array Properties.oy/agg_function.py
+++ NumPy/Chapter 2 Array Properties & Operations/Numpy Array Properties.oy/agg_function.py
@@ -2,4 +2,14 @@ import numpy as np
 
 arr = np.array([10,20,30,40])
 
-print(np.sum(arr))
+print(np.sum(arr))     # Sum of all elements
+
+print(np.mean(arr))    # Mean of all elements
+
+print(np.std(arr))     # Standard deviation of elements
+
+print(np.min(arr))     # Minimum element
+
+print(np.max(arr))     # Maximum element
+
+print(np.var(arr))   # Variance of elements
