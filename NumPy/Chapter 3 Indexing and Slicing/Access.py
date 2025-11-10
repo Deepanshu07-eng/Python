ebd96e77fb28804a774ef419a80b6efd9307106a
Deepanshu <d2004gautam@gmail.com>
2025-11-10 15:09:16 +0530

    Index error

--- NumPy/Chapter 3 Indexing and Slicing/Access.py
+++ NumPy/Chapter 3 Indexing and Slicing/Access.py
@@ -12,3 +12,7 @@ arr = np.array([10,20,30,40,50])
 print(arr[0]) #first element
 print(arr[2]) #third element
 print(arr[-1])  #last element
+
+'''
+If i do -> print(arr[10]) it raises an IndexError because index 10 is out of bounds for array of size 5.
+'''
