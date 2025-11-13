ce28467654aa91839b3d103557bc7ee7a46b2cad
Deepanshu <d2004gautam@gmail.com>
2025-11-13 22:32:33 +0530

    Brodcasting from 1d t 2d and its error point

---
+++ NumPy/Chapter 5 Broadcasting/3 single.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+arr = np.array([10, 20, 30])
+
+result = arr *2
+result2 = arr %5
+result3 = arr * 421
+result4 = arr / 7
+result5 = arr + 213241
+
+print(result)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
---
+++ NumPy/Chapter 5 Broadcasting/Brod from 1d to 2d.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+matrix = np.array([[42,52,74],[13,24,36]])
+
+vector = np.array([10,20,30])
+
+result = matrix + vector
+print(result)
