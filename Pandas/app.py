ce0db7363e1a340a81dbedb6716d3295357df3db
Deepanshu <d2004gautam@gmail.com>
2025-11-15 19:21:36 +0530

    Reading excel dataset

--- Pandas/app.py
+++ Pandas/app.py
@@ -5,3 +5,9 @@ import pandas as pd
 df = pd.read_csv("customers-1000.csv")  #use encoding = "latin1" if needed for special characters or error in terminal
 
 print(df)
+
+# read excel data set
+
+xl = pd.read_excel("superstore.xls", encoding = "latin1" )
+
+print(xl)
