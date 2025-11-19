2d969dc0695e27a7680e9dfade2c412829d4bbae
Deepanshu <d2004gautam@gmail.com>
2025-11-19 18:34:23 +0530

    Displaying first 1- rows and last 10 rows

---
+++ Pandas/rows.py
@@ -0,0 +1,13 @@
+# head() #tail()
+#head(n), head() = 5 rows
+#rail(n), tail() = 5 rows
+
+import pandas as pd
+df = pd.read_csv("customers-1000.csv")
+
+print('Displaying first 10 rows of the dataframe')
+print(df.head(10))
+
+print('\nDisplaying last 10 rows of the dataframe')
+print(df.tail(10))
+
