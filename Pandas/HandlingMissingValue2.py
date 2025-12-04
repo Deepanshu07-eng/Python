de3f5d5f4ae79cb2fb83443209d7e6055ba311b4
Deepanshu <d2004gautam@gmail.com>
2025-12-04 06:08:03 +0530

    Fillna()

---
+++ Pandas/HandlingMissingValue2.py
@@ -0,0 +1,19 @@
+#fillna()
+#fillna(value, inplace = True)
+
+import pandas as pd
+
+data = {
+    "Name": ['Rahul', 'Sonia', 'Yash', 'Drishti', None, 'Deepanshu', 'Ankita', 'Karan', 'Simran', 'Amit', 'Neha', 'Rohan', 'Pooja'],
+    "Age" : [23, 24, 22, 21, 25, 23, 24, 22, 26, 21, 27,None,23],
+    "City" : ['Pune', 'Mumbai', 'Bangalore', None,'Delhi', 'Chennai', 'Kolkata', 'Hyderabad', 'Ahmedabad', 'Jaipur', 'Lucknow', 'Surat', 'Indore'],
+    "Salary": [50000, 60000, 55000, 52000, 58000, 62000, 59000,None, 61000, 63000, 54000, 65000, 57000],
+    "Performance score": [85, 90, 88, 82, 87, 91, 89,None, 86, 92, 84, 93, 83]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+
+df.fillna(value=0, inplace = True)
+print(df)
