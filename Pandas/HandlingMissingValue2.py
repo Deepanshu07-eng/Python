0933974436ecf60ab2fe8ead7720a5b2146c7dd0
Deepanshu <d2004gautam@gmail.com>
2025-12-09 23:30:28 +0530

    Handling missing value

--- Pandas/HandlingMissingValue2.py
+++ Pandas/HandlingMissingValue2.py
@@ -7,7 +7,7 @@ data = {
     "Name": ['Rahul', 'Sonia', 'Yash', 'Drishti', None, 'Deepanshu', 'Ankita', 'Karan', 'Simran', 'Amit', 'Neha', 'Rohan', 'Pooja'],
     "Age" : [23, 24, 22, 21, 25, 23, 24, 22, 26, 21, 27,None,23],
     "City" : ['Pune', 'Mumbai', 'Bangalore', None,'Delhi', 'Chennai', 'Kolkata', 'Hyderabad', 'Ahmedabad', 'Jaipur', 'Lucknow', 'Surat', 'Indore'],
-    "Salary": [50000, 60000, 55000, 52000, 58000, 62000, 59000,None, 61000, 63000, 54000, 65000, 57000],
+    "Salary": [50000, 60000,150000, 55000, 52000, 58000, 62000, 59000,None, 61000, 63000, 54000, 65000, 57000],
     "Performance score": [85, 90, 88, 82, 87, 91, 89,None, 86, 92, 84, 93, 83]
 }
 
